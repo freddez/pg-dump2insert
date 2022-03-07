322284c45545f9768da20a29f2a62d7211c390be
Frederic de Zorzi <f@idez.net>
2022-03-07 11:06:17 +0100

    Regex improvement to parse "COPY" line arguments

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ fn main() {
     let mut line = String::new();
     let mut table_name = String::new();
     let mut fields = String::new();
-    let re = Regex::new(r"^COPY ([\w\.]+) \(([\w, ]+)\) FROM stdin;").unwrap();
+    let re = Regex::new(r"^COPY (.+) \((.+)\) FROM stdin;").unwrap();
     let mut insert_mode = false;
     while stdin.read_line(&mut line).unwrap() > 0 {
         if insert_mode {
             if line == "\\.\n" {
                 insert_mode = false;
-            }
-            else {
+            } else {
                 let mut values = String::new();
                 line.pop();
                 for s in line.replace("'", "''").split("\t") {
@@ -30,18 +29,19 @@ fn main() {
                 }
                 values.pop();
                 values.pop();
-                println!("INSERT INTO {} ({}) VALUES ({});",
-                         table_name, fields, values);
+                println!(
+                    "INSERT INTO {} ({}) VALUES ({});",
+                    table_name, fields, values
+                );
             }
-        }
-        else {
+        } else {
             match re.captures(&line) {
                 None => print!("{}", line),
                 Some(caps) => {
                     table_name = String::from(caps.get(1).map_or("", |m| m.as_str()));
                     fields = String::from(caps.get(2).map_or("", |m| m.as_str()));
                     insert_mode = true;
-                },
+                }
             };
         }
         line.clear();
